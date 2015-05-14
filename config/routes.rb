844b16aeaeb8171c5c2a393574fe76fb33fbcf35
Jonathan Martínez <klaha.77@gmail.com>
2015-05-14 21:30:26 +0000

    API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get '/registered_applications/read/:id', to: 'registered_applications#read', as: 'read_app'
   resources :registered_applications, only: [:index, :new, :create, :destroy, :show]
   
+  namespace :api, defaults: { format: :json } do
+    resources :events, only: [:create]
+  end
+  
   devise_for :users
   root to: 'welcome#index'
 end
